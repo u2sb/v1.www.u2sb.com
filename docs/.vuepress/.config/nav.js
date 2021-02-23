681e4abc349b1fadb6f54364cf9f6afc70c080b7
MonoLogueChi <xxwhite.com@gmail.com>
2021-02-23 19:35:41 +0800

    更新

---
+++ docs/.vuepress/.config/nav.js
@@ -0,0 +1,20 @@
+module.exports = [
+  { text: "Home", link: "/" },
+  {
+    text: "开源项目",
+    items: [
+      {
+        text: "弹幕服务器",
+        link: "https://dandoc.u2sb.top/",
+      },
+      {
+        text: "MetingJS.Server",
+        link: "/open-source/",
+      },
+      {
+        text: "hexo-tag-mmedia",
+        link: "/open-source/",
+      },
+    ],
+  },
+];
---
+++ docs/01.OpenSource/index.md
@@ -0,0 +1,16 @@
+---
+pageComponent:
+  name: Catalogue
+  data:
+    key: 01.OpenSource
+    imgUrl: /img/tou.png
+    description: 我们的开源项目
+title: OpenSource
+date: 2020-01-27 21:50:53
+permalink: /open-source/
+sidebar: true
+article: false
+comment: false
+comments: false
+editLink: false
+---
