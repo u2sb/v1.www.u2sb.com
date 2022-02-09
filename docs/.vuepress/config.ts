801e27859acdaa9b86b751a95e5f7baf94fe7231
MonoLogueChi <xxwhite.com@gmail.com>
2022-02-09 11:09:18 +0800

    更新配置示例

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,24 +1,16 @@
 import { defineConfig4CustomTheme } from "vuepress/config";
 import { VdoingThemeConfig } from "vuepress-theme-vdoing/types";
-import themeConfig from "./.config/themeConfig";
-import markdown from "./.config/markdown";
-import plugins from "./.config/plugins";
+import themeConfig from "./config/themeConfig";
+import markdown from "./config/markdown";
+import plugins from "./config/plugins";
+import head from "./config/head";
 
 export default defineConfig4CustomTheme<VdoingThemeConfig>({
   title: "u2sb",
   description: "You Are Shuai Bi",
   dest: "dist",
   shouldPrefetch: () => false,
-  head: [
-    ["link", { rel: "icon", href: "/favicon.ico" }],
-    [
-      "meta",
-      {
-        name: "viewport",
-        content: "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
-  ],
+  head: head,
   theme: "vdoing",
   themeConfig: themeConfig,
   markdown: markdown,
--- docs/.vuepress/config/themeConfig.ts
+++ docs/.vuepress/config/themeConfig.ts
@@ -1,6 +1,7 @@
 import nav from "./nav";
+import { VdoingThemeConfig } from "vuepress-theme-vdoing/types";
 
-export default {
+export default <VdoingThemeConfig>{
   nav: nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: "/assets/img/logo.png",
