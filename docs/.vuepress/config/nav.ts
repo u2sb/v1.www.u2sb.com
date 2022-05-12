1d74d65b44976e1053d6e0cef33bfa31299f1965
MonoLogueChi <xxwhite.com@gmail.com>
2022-05-12 17:30:25 +0800

    更新硬件介绍

--- docs/.vuepress/config/nav.ts
+++ docs/.vuepress/config/nav.ts
@@ -7,20 +7,29 @@ export default <Array<NavItem>>[
     items: [
       {
         text: "弹幕服务器",
-        link: "https://dandoc.u2sb.com/"
+        link: "https://dandoc.u2sb.com/",
       },
       {
         text: "MetingJS.Server",
-        link: "/open-sw/#MetingJsServer"
+        link: "/open-sw/#MetingJsServer",
       },
       {
         text: "vuepress-plugin-smplayer",
-        link: "/open-sw/#vuepress-plugin-smplayer"
+        link: "/open-sw/#vuepress-plugin-smplayer",
       },
       {
         text: "hexo-tag-mmedia",
-        link: "/open-sw/#hexo-tag-mmedia"
-      }
-    ]
-  }
+        link: "/open-sw/#hexo-tag-mmedia",
+      },
+    ],
+  },
+  {
+    text: "开源硬件",
+    items: [
+      {
+        text: "OBS导播键盘",
+        link: "/open-hw/#OBS导播键盘",
+      },
+    ],
+  },
 ];
